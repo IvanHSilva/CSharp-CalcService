6f765ca37991a4e748a2225441572bb91bb4d76e
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-09-12 11:37:23 -0300

    Service class and simple solution created

--- CalcService/Program.cs
+++ CalcService/Program.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿using CalcService.Services;
+using System;
+using System.Collections.Generic;
 
 namespace CalcService {
     internal class Program {
         static void Main(string[] args) {
+            
+            List<int> list = new List<int>();
+
+            Console.Write("Quantidade de itens: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.Write($"Item {i}: ");
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calc = new CalculationService();
+
+            int max = calc.Max(list);
+
+            Console.WriteLine();
+            Console.WriteLine($"Máximo: {max}");
+            Console.WriteLine();
         }
     }
 }
